2ffa1c002681304a6d2013ca395b4459ee82fef6
tulayer <tulayer20@gmail.com>
2022-08-23 18:31:24 +0300

    added ByeWorld.java

---
+++ src/first/ByeWorld.java
@@ -0,0 +1,8 @@
+package first;
+
+public class ByeWorld {
+    public static void main(String[] args) {
+        System.out.println("Maybe 5 weeks of Java left");
+        System.out.println("1.5 weeks of git");
+    }
+}
